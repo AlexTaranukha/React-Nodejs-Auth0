921320a26ecaf684d534e3a36e7fcffd360e5851
ataranukha <ataranukha@cars.com>
2019-01-16 13:45:11 -0600

    Added error handling to re-authentication

--- frontend/src/SecuredRoute/SecuredRoute.js
+++ frontend/src/SecuredRoute/SecuredRoute.js
@@ -3,9 +3,12 @@ import {Route} from 'react-router-dom';
 import auth0Client from '../Auth';
 
 function SecuredRoute(props) {
-  const {component: Component, path} = props;
+  const {component: Component, path, checkingSession} = props;
   return (
     <Route path={path} render={ () => {
+      // if session is being checked
+      if (checkingSession) return <h3 classNmae="text-center">Validating session...</h3>
+      // otherwise
       if (!auth0Client.isAuthenticated()) {
         auth0Client.signIn();
         return <div></div>;
